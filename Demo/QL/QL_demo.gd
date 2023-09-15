acbf4b4c2c1b7b246273635dbfcd96b1a061f27c
Ryash <ryash072007@gmail.com>
2023-09-15 12:31:09 +0400

    debug data and q-learning done

--- Demo/QL/QL_demo.gd
+++ Demo/QL/QL_demo.gd
@@ -10,17 +10,42 @@ var punish_states = [3, 18, 21, 28, 31]
 var current_state: int = 0
 var previous_reward: float = 0.0
 
+var total_iteration_rewards: Array[float] = []
+var current_iteration_rewards: float = 0.0
 var done: bool = false
 
 func _ready() -> void:
 	qnet = QLearning.new(36, 4)
+	qnet.print_debug_info = true
 
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("predict"):
 		$control.wait_time = 0.5
 	elif Input.is_action_just_pressed("ui_down"):
 		$control.wait_time = 0.02
+	if done:
+		reset()
+		return
+	current_state = row * 6 + column
+	var action_to_do: int = qnet.predict(current_state, previous_reward)
+	
+	current_iteration_rewards += previous_reward
+	previous_reward = 0.0
+		
+	if is_out_bound(action_to_do):
+		previous_reward -= 0.75
+		done = true
+	elif row * 6 + column in punish_states:
+		previous_reward -= 0.5
+		done = true
+	elif (row * 6 + column) in reward_states:
+		previous_reward += 1.0
+		done = true
+	else:
+		previous_reward -= 0.05
+	$player.position = Vector2(96 * column + 16, 512 - (96 * row + 16))
+	$lr.text = str(qnet.exploration_probability)
 
 
 func is_out_bound(action: int) -> bool:
@@ -46,27 +71,7 @@ func reset():
 	row = 0
 	column = 0
 	done = false
+	print(current_iteration_rewards)
+	total_iteration_rewards.append(current_iteration_rewards)
+	current_iteration_rewards = 0.0
 	$player.position = Vector2(96 * column + 16, 512 - (96 * row + 16))
-
-
-func _on_control_timeout() -> void:
-	if done:
-		reset()
-		return
-	current_state = row * 6 + column
-	var action_to_do: int = qnet.predict(current_state, previous_reward)
-	previous_reward = 0.0
-	if is_out_bound(action_to_do):
-		previous_reward -= 0.75
-		done = true
-	elif row * 6 + column in punish_states:
-		previous_reward -= 0.5
-		done = true
-	elif (row * 6 + column) in reward_states:
-		previous_reward += 1.0
-		print("Noice")
-		done = true
-	else:
-		previous_reward -= 0.05
-	$player.position = Vector2(96 * column + 16, 512 - (96 * row + 16))
-	$lr.text = str(qnet.exploration_probability)
--- addons/neural_net/Scripts/QLearning/QLearning.gd
+++ addons/neural_net/Scripts/QLearning/QLearning.gd
@@ -10,13 +10,12 @@ var QTable: Matrix
 
 # Hyper-parameters
 var exploration_probability: float = 1.0 # The probability that the agent will either explore or exploit the QTable
-var exploration_decreasing_decay: float = 0.005 # The exploartion decreasing decay for exponential decreasing
+var exploration_decreasing_decay: float = 0.01 # The exploartion decreasing decay for exponential decreasing
 var min_exploration_probability: float = 0.01 # The least value that the exploration_probability can fall to
 var discounted_factor: float = 0.9 # Basically the gamma
-var learning_rate: float = 0.15 # How fast the agent learns
-var decay_per_steps: int = 250
-var iterations_completed: int = 0
-# Stored Data
+var learning_rate: float = 0.2 # How fast the agent learns
+var decay_per_steps: int = 100
+var steps_completed: int = 0
 
 # States
 var previous_state: int = -100# To be used in the algorithms
@@ -24,15 +23,15 @@ var current_state: int # To be swapped for the previous state at the end of each
 var previous_action: int # To b usd in the algorithm
 
 var done: bool = false
-
 var is_learning: bool = true
+var print_debug_info: bool = false
 
 func _init(n_observations: int, n_action_spaces: int, _is_learning: bool = true) -> void:
-	self.observation_space = n_observations
-	self.action_spaces = n_action_spaces
-	self.is_learning = _is_learning
+	observation_space = n_observations
+	action_spaces = n_action_spaces
+	is_learning = _is_learning
 	
-	self.QTable = Matrix.new(observation_space, action_spaces)
+	QTable = Matrix.new(observation_space, action_spaces)
 
 func predict(current_state: int, reward_of_previous_state: float) -> int:
 	if is_learning:
@@ -45,7 +44,7 @@ func predict(current_state: int, reward_of_previous_state: float) -> int:
 	# If randf is lesser than the exploration probability, which will be the case initially, 
 	# it chooses a random value, else, it exploits the QTable
 	if randf() < exploration_probability and is_learning:
-		action_to_take = randi_range(0, self.action_spaces - 1)
+		action_to_take = randi_range(0, action_spaces - 1)
 	else:
 		action_to_take = QTable.index_of_max_from_row(current_state)
 	
@@ -53,12 +52,14 @@ func predict(current_state: int, reward_of_previous_state: float) -> int:
 		previous_state = current_state
 		previous_action = action_to_take
 	
-		if iterations_completed != 0 and iterations_completed % decay_per_steps == 0:
+		if steps_completed != 0 and steps_completed % decay_per_steps == 0:
 			exploration_probability = maxf(min_exploration_probability, exploration_probability - exploration_decreasing_decay)#, exp(-exploration_decreasing_decay * exp(1)))
-			# #
-			print(exploration_probability)
-			print(QTable.data)
-			print("****************************************************************")
-		
-	iterations_completed += 1
+	
+	if print_debug_info and steps_completed % decay_per_steps == 0:
+		print("Total steps completed:", steps_completed)
+		print("Current exploration probability:", exploration_probability)
+		print("Q-Table data:", QTable.data)
+		print("-----------------------------------------------------------------------------------------")
+	
+	steps_completed += 1
 	return action_to_take
