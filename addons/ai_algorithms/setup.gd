6b1951201c4f0d97d5fd9849260b4ce0b913b05b
Yash Raj <ryash072007@gmail.com>
2024-07-18 10:30:06 +0400

    addon name changed and olders created

---
+++ addons/ai_algorithms/setup.gd
@@ -0,0 +1,10 @@
+@tool
+extends EditorPlugin
+
+var Neural_Net = load("res://addons/ai_algorithms/Scripts/Neural/Neural_Net.gd")
+var logo: CompressedTexture2D = load("res://addons/ai_algorithms/NeuralNetwork.png")
+
+func _enter_tree():
+	add_custom_type("Neural_Net", "Node2D", Neural_Net, logo)
+func _exit_tree():
+	remove_custom_type("Neural_Net")
