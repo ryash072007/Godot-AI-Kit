6bf3cb1817f79eedf3447f18482106dd3de7df62
Yash Raj <ryash072007@gmail.com>
2025-01-02 14:15:58 +0400

    random positions no longer spawn on the agent

--- Demo/PPO/ppo_demo.gd
+++ Demo/PPO/ppo_demo.gd
@@ -12,19 +12,24 @@ func _ready() -> void:
 	reset_env()
 
 func reset_env() -> void:
-	obstacle_tile = Vector2i(
-		randi_range(top_left_corner.x, bottom_right_corner.x),
-		randi_range(top_left_corner.y, bottom_right_corner.y)
-		)
+	obstacle_tile = get_valid_random_position()
 
 	while true:
-		goal_tile = Vector2i(
-			randi_range(top_left_corner.x, bottom_right_corner.x),
-			randi_range(top_left_corner.y, bottom_right_corner.y)
-			)
+		goal_tile = get_valid_random_position()
 		if obstacle_tile != goal_tile:
 			break
 
 
 	WorldTileMap.set_cell(0, obstacle_tile, tile.OBSTACLE, Vector2i.ZERO, 1)
 	WorldTileMap.set_cell(0, goal_tile, tile.GOAL, Vector2i.ZERO, 1)
+
+func get_valid_random_position() -> Vector2i:
+	var pos: Vector2i
+	while true:
+		pos = Vector2i(
+				randi_range(top_left_corner.x, bottom_right_corner.x),
+				randi_range(top_left_corner.y, bottom_right_corner.y)
+				)
+		if not (pos.x in [8,9] and pos.y in [4,5]):
+			break
+	return pos
