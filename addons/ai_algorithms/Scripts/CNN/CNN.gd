1222ca47b90647fcb130c11fceef9e67d9f18a8a
Yash Raj <ryash072007@gmail.com>
2025-01-06 18:00:50 +0400

    added optimised init and forward pass for dense layer

--- addons/ai_algorithms/Scripts/CNN/CNN.gd
+++ addons/ai_algorithms/Scripts/CNN/CNN.gd
@@ -5,11 +5,12 @@ class Layer:
         var filter: Matrix
         var bias: float = 0.0
         var stride: int = 1
-        var activation: String = "RELU"
         var input_shape: Vector2i = Vector2(0, 0)
         var filter_shape: Vector2i = Vector2(0, 0)
         var output_shape: Vector2i = Vector2(0, 0)
         var output: Matrix
+
+        var activationFunction := Activation.RELU
 
         func _init(_input_shape: Vector2i, _output_shape: Vector2i, _filter_shape: Vector2i = Vector2i(3,3), _stride: int = 1) -> void:
             input_shape = _input_shape
@@ -39,8 +40,30 @@ class Layer:
         var output_shape: int = 0
         var output: Matrix
 
+        var ACTIVATIONS: Activation = Activation.new()
+        var activationFunction
+
         func _init(_input_shape: int, _output_shape: int, _activation: String = "RELU") -> void:
             input_shape = _input_shape
             output_shape = _output_shape
             activation = _activation
+        
+            activationFunction = ACTIVATIONS.get(activation)
+
+            if activation in ["RELU", "LEAKYRELU", "ELU", "LINEAR"]:
+                weights = Matrix.uniform_he_init(Matrix.new(output_shape, input_shape), input_shape)
+            elif activation in ["SIGMOID", "TANH"]:
+                weights = Matrix.uniform_glorot_init(Matrix.new(output_shape, input_shape), input_shape, output_shape)
+            else:
+                weights = Matrix.rand(Matrix.new(output_shape, input_shape))
+            
+            biases = Matrix.new(output_shape, 1)
+        
+        # Different from NNA as there input was transposed and so dot product was used
+        func forward(input: Matrix) -> Matrix:
+            output = Matrix.add(Matrix.multiply(input, weights), biases)
+            output = Matrix.map(output, activationFunction.function)
+            return output
+        
+        
 
