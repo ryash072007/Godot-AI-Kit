b4f1d265a63c60e7b721e64623aab167bb72fcae
Yash Raj <ryash072007@gmail.com>
2025-03-04 14:07:56 +0400

    random testing

--- Demo/Random Testing/random_testing.gd
+++ Demo/Random Testing/random_testing.gd
@@ -2,7 +2,12 @@ extends Node2D
 
 
 func _ready() -> void:
-	SDQN.convert("user://ADAM_001_ELU - Copy.ryash")
+	var data: Matrix = Matrix.new(4,4)
+	data.data[0][0] = 1
+	data.data[3][3] = 2
+
+	data = Augmentations.rotate_random(data)
+	print(data.data)
 
 class test:
 	static func function():
